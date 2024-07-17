dc7b3b092fccaee1a844d430a564c34070ebf28f
Ikkxeer <137718457+ikkxeer@users.noreply.github.com>
2024-07-17 13:18:33 +0200

    Add files via upload

---
+++ Scripts/Remove-Secure.ps1
@@ -0,0 +1,41 @@
+Add-Type -AssemblyName System.Windows.Forms
+
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$OpenFileDialog.Filter = "Todos los archivos (*.*)|*.*"
+$OpenFileDialog.Multiselect = $false
+
+if ($OpenFileDialog.ShowDialog() -eq 'OK') {
+    $SelectedFilePath = $OpenFileDialog.FileName
+    Write-Host "File selected: $SelectedFilePath" -ForegroundColor Green
+    Write-Host " "
+} else {
+    Write-Host "No se seleccionó ningún archivo." -ForegroundColor Yellow
+    Write-Host " "
+}
+
+
+function Secure-DeleteFile {
+    param (
+        [string]$FilePath
+    )
+
+    if (Test-Path $FilePath) {
+        $size = (Get-Item $FilePath).length
+        Write-Host "The file size is: $size" -ForegroundColor Yellow
+        Write-Host " "
+        $randomBytes = New-Object byte[] $size
+        (New-Object Random).NextBytes($randomBytes)
+        [System.IO.File]::WriteAllBytes($FilePath, $randomBytes)
+        Write-Host "Processing file to Untraceable File..." -ForegroundColor Yellow
+        Write-Host " "
+        Remove-Item -Path $FilePath -Force
+        Write-Host "File completely deleted from memory!" -ForegroundColor Green
+        Write-Host " "
+    } else {
+        Write-Host "File does not exist: $FilePath" -ForegroundColor Red
+        Write-Host " "
+    }
+}
+
+# Call function with the file selected
+Secure-DeleteFile -FilePath $SelectedFilePath
