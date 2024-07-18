eda15c97306737b4039c875689882b545329b2bc
Ikkxeer <137718457+ikkxeer@users.noreply.github.com>
2024-07-18 12:49:41 +0200

    Update Convert-Image-To-PDF.ps1

--- Scripts/Convert-Image-To-PDF.ps1
+++ Scripts/Convert-Image-To-PDF.ps1
@@ -2,30 +2,30 @@ Add-Type -AssemblyName System.Drawing
 Add-Type -AssemblyName System.Windows.Forms
 
 $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-$OpenFileDialog.Filter = "Images (*.jpg)|*.jpg"
+$FileDialog.Filter = "Images|*.bmp;*.jpg;*.jpeg;*.png;*.gif;*.tiff;*.ico"
 $OpenFileDialog.Multiselect = $false
 
-if ($OpenFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
-    $imgPath = $OpenFileDialog.FileName
-    Write-Host "File selected: $imgPath" -ForegroundColor Green
+if ($FileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+    $selectedImagePath = $FileDialog.FileName
+    Write-Host "File selected: $selectedImagePath" -ForegroundColor Green
     Write-Host " "
 } else {
-    Write-Host "No file selected." -ForegroundColor Yellow
+    Write-Host "No File selected." -ForegroundColor Yellow
     Write-Host " "
     return
 }
 
-$doc = New-Object System.Drawing.Printing.PrintDocument
-$doc.PrinterSettings = New-Object System.Drawing.Printing.PrinterSettings
-$doc.PrinterSettings.PrinterName = "Microsoft Print to PDF"
-$doc.PrinterSettings.PrintToFile = $true
-$outFile = [System.IO.Path]::ChangeExtension($imgPath, ".pdf")
-$doc.PrinterSettings.PrintFileName = $outFile
+$printDocument = New-Object System.Drawing.Printing.PrintDocument
+$printDocument.PrinterSettings = New-Object System.Drawing.Printing.PrinterSettings
+$printDocument.PrinterSettings.PrinterName = "Microsoft Print to PDF"
+$printDocument.PrinterSettings.PrintToFile = $true
+$outputFilePath = [System.IO.Path]::ChangeExtension($selectedImagePath, ".pdf")
+$printDocument.PrinterSettings.PrintFileName = $outputFilePath
 
-$script:_pageIndex = 0
-$doc.add_PrintPage({
+$pageIndex = 0
+$printDocument.add_PrintPage({
     param($sender, [System.Drawing.Printing.PrintPageEventArgs] $e)
-    $image = [System.Drawing.Image]::FromFile($imgPath)
+    $image = [System.Drawing.Image]::FromFile($selectedImagePath)
     try {
         $e.Graphics.DrawImage($image, $e.PageBounds)
         $e.HasMorePages = $false
@@ -34,7 +34,6 @@ $doc.add_PrintPage({
     }
 })
 
-$doc.PrintController = New-Object System.Drawing.Printing.StandardPrintController
-
-$doc.Print()
-Write-Host "File printed to $outFile" -ForegroundColor Green
+$printDocument.PrintController = New-Object System.Drawing.Printing.StandardPrintController
+$printDocument.Print()
+Write-Host "File saved in $outputFilePath" -ForegroundColor Green
