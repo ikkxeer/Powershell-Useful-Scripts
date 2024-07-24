ee65dadbfe8fb59f348749c94e91134617d5c500
Ikkxeer <137718457+ikkxeer@users.noreply.github.com>
2024-07-24 16:47:18 +0200

    Update Encrypt-Password.ps1

--- Scripts/Others/Encrypt-Password.ps1
+++ Scripts/Others/Encrypt-Password.ps1
@@ -1,8 +1,4 @@
-$PassowrdInput = Read-Host "Write your password"
-
-$PasswordText = $PassowrdInput
-$PasswordSecureString = ConvertTo-SecureString $PasswordText -AsPlainText -Force
+$PasswordSecureString = Read-Host "Write your password" -AsSecureString
 $PasswordEncrypted = ConvertFrom-SecureString -SecureString $PasswordSecureString
 $PasswordPath = ".\Password.txt"
 $PasswordEncrypted | Out-File -FilePath $PasswordPath
-Write-Host "The Encrypted password have been saved in $PasswordPath!" -ForegroundColor Green
