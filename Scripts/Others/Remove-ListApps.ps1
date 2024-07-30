411f0dae8e531efef9d698b799f47379a87ca71f
Ikkxeer <137718457+ikkxeer@users.noreply.github.com>
2024-07-30 10:05:23 +0200

    Add files via upload

---
+++ Scripts/Others/Remove-ListApps.ps1
@@ -0,0 +1,12 @@
+$desktopAppsToRemove = @(
+    "APP1",
+    "APP2"
+    "APP3"
+    "APP4"
+)
+
+foreach ($app in $desktopAppsToRemove) {
+    Write-Output "Uninstalling $app using winget..."
+    winget uninstall $app
+    Write-Host "$app has been uninstalled!" -ForegroundColor Green
+}
